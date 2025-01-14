68baec985245f253c08594546ae375baf21c0271
micropuma <micropuma@163.com>
2025-01-14 09:55:48 +0800

    add a new version of build

---
+++ build-cuda-temp.sh
@@ -0,0 +1,9 @@
+cmake -G Ninja -B ./build/ -S . \
+    -DIREE_TARGET_BACKEND_CUDA=ON \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DIREE_ENABLE_ASSERTIONS=ON \
+    -DCMAKE_CXX_FLAGS="-Wno-error=dangling-pointer" \
+    -DIREE_HAL_DRIVER_CUDA=ON \
+    -DIREE_BUILD_PYTHON_BINDINGS=ON  \
+    -DPython3_EXECUTABLE="$(which python3)" \
+    -DIREE_CUDA_LIBDEVICE_PATH=/usr/local/cuda-12.1/nvvm/libdevice/libdevice.10.bc
