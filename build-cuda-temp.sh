0374028709469e28c315000c0e0b404a0b12cb3c
micropuma <micropuma@163.com>
2025-01-21 15:24:32 +0800

    add some new config

--- build-cuda-temp.sh
+++ build-cuda-temp.sh
@@ -1,7 +1,7 @@
 cmake -G Ninja -B ./build/ -S . \
-    -DIREE_TARGET_BACKEND_CUDA=ON \
     -DCMAKE_BUILD_TYPE=Debug \
     -DIREE_ENABLE_ASSERTIONS=ON \
+    -DIREE_TARGET_BACKEND_CUDA=ON \
     -DCMAKE_CXX_FLAGS="-Wno-error=dangling-pointer" \
     -DIREE_HAL_DRIVER_CUDA=ON \
     -DIREE_BUILD_PYTHON_BINDINGS=ON  \
