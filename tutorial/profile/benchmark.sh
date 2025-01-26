70313493ca99962b0a7c3673e1c24ad10e916bbe
micropuma <micropuma@163.com>
2025-01-26 20:22:42 +0800

    compile & benchmark scripts

---
+++ tutorial/profile/benchmark.sh
@@ -0,0 +1,6 @@
+# 测试的这个函数不需要输入
+iree-benchmark-module --device=cuda \
+  --module=linalg.vmfb \
+  --input=10xf32=[0,1,2,3,4,5,6,7,8,9] \
+  --input=10xf32=[90,80,70,60,50,40,30,20,10,0] \
+  --function=foo
