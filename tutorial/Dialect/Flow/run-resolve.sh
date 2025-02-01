d75ff03fc9aebf23ef0bb5d0b7558665085844c4
micropuma <micropuma@163.com>
2025-02-01 17:34:06 +0800

    two test scripts

---
+++ tutorial/Dialect/Flow/run-resolve.sh
@@ -0,0 +1,3 @@
+iree-opt -resolve-ranked-shaped-type-result-dims \
+	 -split-input-file \
+	 ./resolve_dim_ops.mlir 
---
+++ tutorial/Dialect/Flow/tensor2flow.sh
@@ -0,0 +1,1 @@
+iree-opt --allow-unregistered-dialect --split-input-file --iree-flow-convert-to-flow ./fill.mlir 
