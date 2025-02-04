aafb5157edbc9cae4548b671c606ec1cff7e1b18
micropuma <micropuma@163.com>
2025-02-04 21:39:06 +0800

    add comments for kernel3

--- tutorial/GPU/cuda/SGEMM_CUDA/src/kernels/3_kernel_shared_mem_blocking.cuh
+++ tutorial/GPU/cuda/SGEMM_CUDA/src/kernels/3_kernel_shared_mem_blocking.cuh
@@ -13,19 +13,23 @@ __global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha,
                                        const float *A, const float *B,
                                        float beta, float *C) {
   // the output block that we want to compute in this threadblock
+  // a block has 32x32 threads，where blocksize is 32
   const uint cRow = blockIdx.x;
   const uint cCol = blockIdx.y;
 
   // allocate buffer for current block in fast shared mem
   // shared mem is shared between all threads in a block
+  // explicit cache buffer for block
   __shared__ float As[BLOCKSIZE * BLOCKSIZE];
   __shared__ float Bs[BLOCKSIZE * BLOCKSIZE];
 
   // the inner row & col that we're accessing in this thread
+  // in this model, threads are one dimension
   const uint threadCol = threadIdx.x % BLOCKSIZE;
   const uint threadRow = threadIdx.x / BLOCKSIZE;
 
   // advance pointers to the starting positions
+  // a block is 32x32, so blockIdx.x and blockIdx.y must multiply BLOCKSIZE
   A += cRow * BLOCKSIZE * K;                    // row=cRow, col=0
   B += cCol * BLOCKSIZE;                        // row=0, col=cCol
   C += cRow * BLOCKSIZE * N + cCol * BLOCKSIZE; // row=cRow, col=cCol
@@ -35,6 +39,8 @@ __global__ void sgemm_shared_mem_block(int M, int N, int K, float alpha,
     // Have each thread load one of the elements in A & B
     // Make the threadCol (=threadIdx.x) the consecutive index
     // to allow global memory access coalescing
+    // 1. caching a whole block
+    // 2. utilize global memory access coalescing
     As[threadRow * BLOCKSIZE + threadCol] = A[threadRow * K + threadCol];
     Bs[threadRow * BLOCKSIZE + threadCol] = B[threadRow * N + threadCol];
 
