06f5f53c5f9889830f65cd136a5de373f5668803
micropuma <micropuma@163.com>
2025-02-12 15:19:01 +0800

    a test script

--- tutorial/GPU/run.sh
+++ tutorial/GPU/run.sh
@@ -1,6 +1,5 @@
-iree-compile 
-  --iree-hal-target-backends=cuda \
-  --iree-hal-cuda-llvm-target-arch=sm_86\
+iree-compile --iree-hal-target-backends=cuda \
+  --iree-cuda-target=sm_86 \
   matmul.mlir \
   --mlir-disable-threading \
   --mlir-elide-elementsattrs-if-larger=10 \
