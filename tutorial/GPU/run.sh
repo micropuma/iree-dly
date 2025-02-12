c558f23b38691263c2ee1ba80b55c87d19bf351b
micropuma <micropuma@163.com>
2025-02-12 17:53:38 +0800

    TensorCore debug pipeline script and dump codes

--- tutorial/GPU/run.sh
+++ tutorial/GPU/run.sh
@@ -1,6 +1,7 @@
 iree-compile --iree-hal-target-backends=cuda \
   --iree-cuda-target=sm_86 \
-  matmul.mlir \
   --mlir-disable-threading \
   --mlir-elide-elementsattrs-if-larger=10 \
-  -o test.vmfb
+  --mlir-print-ir-after-all \
+  matmul.mlir -o test.vmfb \
+  2>&1 | tee output.dump
